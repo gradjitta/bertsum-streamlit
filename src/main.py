f0f75bf64f82268e78dcb7bcccea8f2c5ddbbff3
Aditya Jitta <aditya.jitta@selko.io>
2020-07-04 22:45:46 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,12 +7,45 @@ from numpy import ndarray
 from sklearn.cluster import KMeans
 from spacy.lang.en import English
 from transformers import *
+import streamlit as st
 
 logging.basicConfig(level=logging.WARNING)
 import spacy
 
 device = "cpu"
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem; margin-bottom: 2.5rem">{}</div>"""
+
+default_value_goes_here = """
+New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York. 
+A year later, she got married again in Westchester County, but to a different man and without divorcing her first husband. 
+Only 18 days after that marriage, she got hitched yet again. Then, Barrientos declared "I do" five more times, sometimes only within two weeks of each other. 
+In 2010, she married once more, this time in the Bronx. In an application for a marriage license, she stated it was her "first and only" marriage. 
+Barrientos, now 39, is facing two criminal counts of "offering a false instrument for filing in the first degree," referring to her false statements on the 
+2010 marriage license application, according to court documents. 
+Prosecutors said the marriages were part of an immigration scam. 
+On Friday, she pleaded not guilty at State Supreme Court in the Bronx, according to her attorney, Christopher Wright, who declined to comment further. 
+After leaving court, Barrientos was arrested and charged with theft of service and criminal trespass for allegedly sneaking into the New York subway through an emergency exit, said Detective 
+Annette Markowski, a police spokeswoman. In total, Barrientos has been married 10 times, with nine of her marriages occurring between 1999 and 2002. 
+All occurred either in Westchester County, Long Island, New Jersey or the Bronx. She is believed to still be married to four men, and at one time, she was married to eight men at once, prosecutors say. 
+Prosecutors said the immigration scam involved some of her husbands, who filed for permanent residence status shortly after the marriages. 
+Any divorces happened only after such filings were approved. It was unclear whether any of the men will be prosecuted. 
+The case was referred to the Bronx District Attorney\'s Office by Immigration and Customs Enforcement and the Department of Homeland Security\'s 
+Investigation Division. Seven of the men are from so-called "red-flagged" countries, including Egypt, Turkey, Georgia, Pakistan and Mali. 
+Her eighth husband, Rashid Rajput, was deported in 2006 to his native Pakistan after an investigation by the Joint Terrorism Task Force. 
+If convicted, Barrientos faces up to four years in prison.  Her next court appearance is scheduled for May 18.
+"""
+
+class SentenceExtractor(object):
+    def __init__(self, language=English):
+        self.nlp = language()
+        self.nlp.add_pipe(self.nlp.create_pipe('sentencizer'))
+
+    def process(self, body: str, min_length: int = 40, max_length: int = 600) -> List[str]:
+        doc = self.nlp(body)
+        return [c.string.strip() for c in doc.sents if max_length > len(c.string.strip()) > min_length]
+
+    def __call__(self, body: str, min_length: int = 40, max_length: int = 600) -> List[str]:
+        return self.process(body, min_length, max_length)
 
 def extract_embeddings(text, polling_type = 'mean'):
     tokenized_text = tokenizer.tokenize(text)
@@ -59,21 +92,11 @@ def generate_summary(sentences, text_embeddings, ratio_value):
         hidden_args.insert(0,0)
     return [sentences[j] for j in hidden_args]
 
-class SentenceExtractor(object):
-    def __init__(self, language=English):
-        self.nlp = language()
-        self.nlp.add_pipe(self.nlp.create_pipe('sentencizer'))
-
-    def process(self, body: str, min_length: int = 40, max_length: int = 600) -> List[str]:
-        doc = self.nlp(body)
-        return [c.string.strip() for c in doc.sents if max_length > len(c.string.strip()) > min_length]
-
-    def __call__(self, body: str, min_length: int = 40, max_length: int = 600) -> List[str]:
-        return self.process(body, min_length, max_length)
 
 
-import streamlit as st
-
+###
+### Main UI
+###
 
 @st.cache()
 def load_bert(name = 'distilbert-base-cased'):
@@ -92,26 +115,6 @@ model, tokenizer = load_all()
 
 st.title("Summarizarion with DistillBERT clusters")
 st.write('torch version  is', torch.__version__)
-
-default_value_goes_here = """
-New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York. 
-A year later, she got married again in Westchester County, but to a different man and without divorcing her first husband. 
-Only 18 days after that marriage, she got hitched yet again. Then, Barrientos declared "I do" five more times, sometimes only within two weeks of each other. 
-In 2010, she married once more, this time in the Bronx. In an application for a marriage license, she stated it was her "first and only" marriage. 
-Barrientos, now 39, is facing two criminal counts of "offering a false instrument for filing in the first degree," referring to her false statements on the 
-2010 marriage license application, according to court documents. 
-Prosecutors said the marriages were part of an immigration scam. 
-On Friday, she pleaded not guilty at State Supreme Court in the Bronx, according to her attorney, Christopher Wright, who declined to comment further. 
-After leaving court, Barrientos was arrested and charged with theft of service and criminal trespass for allegedly sneaking into the New York subway through an emergency exit, said Detective 
-Annette Markowski, a police spokeswoman. In total, Barrientos has been married 10 times, with nine of her marriages occurring between 1999 and 2002. 
-All occurred either in Westchester County, Long Island, New Jersey or the Bronx. She is believed to still be married to four men, and at one time, she was married to eight men at once, prosecutors say. 
-Prosecutors said the immigration scam involved some of her husbands, who filed for permanent residence status shortly after the marriages. 
-Any divorces happened only after such filings were approved. It was unclear whether any of the men will be prosecuted. 
-The case was referred to the Bronx District Attorney\'s Office by Immigration and Customs Enforcement and the Department of Homeland Security\'s 
-Investigation Division. Seven of the men are from so-called "red-flagged" countries, including Egypt, Turkey, Georgia, Pakistan and Mali. 
-Her eighth husband, Rashid Rajput, was deported in 2006 to his native Pakistan after an investigation by the Joint Terrorism Task Force. 
-If convicted, Barrientos faces up to four years in prison.  Her next court appearance is scheduled for May 18.
-"""
 
 
 user_input = st.text_area("Text to Summarize", default_value_goes_here, height = 400)
